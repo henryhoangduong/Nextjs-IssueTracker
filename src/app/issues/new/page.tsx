401ed820e9f534d2e2f66df507410b8e6c78d308
henryhoangduong <henryhoangduong@gmail.com>
2025-01-21 11:50:03 +0700

    Extract message to component

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { TextField, Button } from "@radix-ui/themes";
+import { TextField, Button, Text } from "@radix-ui/themes";
 import React, { useState } from "react";
 import SimpleMdeReact from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
@@ -8,24 +8,30 @@ import { Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { Callout } from "@radix-ui/themes";
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import z from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
   return (
     <div className="max-w-xl">
       {error && (
         <Callout.Root color="red" className="mb-5">
-         <Callout.Text>
-          {error}
-        </Callout.Text>
+          <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
-       
       )}
       <form
         className="max-w-xl space-y-3"
@@ -39,6 +45,7 @@ const NewIssuePage = () => {
         })}
       >
         <TextField.Root size="3" placeholder="Title" {...register("title")} />
+        {errors.title && <ErrorMessage>{errors.title?.message}</ErrorMessage>}
         <Controller
           name="description"
           control={control}
@@ -46,6 +53,10 @@ const NewIssuePage = () => {
             <SimpleMdeReact placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <ErrorMessage>{errors.description?.message} </ErrorMessage>
+        )}
+
         {/* <SimpleMdeReact  placeholder="Description" */}
         <Button>Submit New Issue</Button>
       </form>
